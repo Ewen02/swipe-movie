fcf6426b17376daf4a9785f97e946917fc552671
Ewen LE QUERE <ewen.le-quere@epitech.eu>
2025-09-06 12:29:32 +0200

    chore(web/storybook): setup Storybook with Vitest integration

---
+++ apps/web/.storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-vitest",
+    "@chromatic-com/storybook",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;
