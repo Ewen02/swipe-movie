c0f592cebd5321f564bc1fae0d2c937db53c96ef
Ewen LE QUERE <ewen.le-quere@epitech.eu>
2025-12-10 01:43:11 +0100

    fix(ui): move toaster position to bottom-right

--- packages/ui/src/organisms/toaster.tsx
+++ packages/ui/src/organisms/toaster.tsx
@@ -11,7 +11,7 @@ interface ToasterProps {
 
 export function Toaster({ toasts, dismiss }: ToasterProps) {
   return (
-    <div className="fixed top-0 right-0 z-50 flex flex-col gap-2 p-4 pointer-events-none">
+    <div className="fixed bottom-0 right-0 z-50 flex flex-col gap-2 p-4 pointer-events-none">
       <AnimatePresence>
         {toasts.map((toast) => (
           <Toast key={toast.id} {...toast} onClose={() => dismiss(toast.id)} />
